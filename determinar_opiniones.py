0ed8bdc27b9f147329d42cc6773560d4a42a002f
Enrique Marina <eherraiz@myuax.com>
2022-10-26 20:56:25 +0200

    Ultimos detalles

--- determinar_opiniones.py
+++ determinar_opiniones.py
@@ -1,5 +1,7 @@
 import re
 import pandas as pd
+
+# El modulo nltk esta en desuso puesto que no funciona bien del todo
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
@@ -15,10 +17,19 @@ from sklearn.model_selection import GridSearchCV
 from sklearn import svm
 
 class Predicciones_cambio_climatico:
-    def __init__(self, name_df):
-        mensajesTwitter = pd.read_csv(name_df, delimiter=";")
+    '''Esta clase nos va a permitir entrenar un modelo a partir de un csv el cual predice si una persona cree o no en el cambio climático a través de una frase
+    '''
+    def __init__(self, name_csv):
+        '''Creamos el constructor a partir del cual vamos a trabajar
 
-    def normalizacion(self):
+        Args:
+            name_csv (String): Nombre del csv
+        '''
+
+
+        self.mensajesTwitter = pd.read_csv(name_csv, delimiter=";")
+
+    def normalizacion(self, mensaje):
         '''Normalizamos el mensaje identificando URL y USER, utilizando caracteres reconocibles(sin acentos) y pasando el texto a minúsculas.
 
         Returns:
@@ -60,10 +71,16 @@ class Predicciones_cambio_climatico:
         self.lemmatizer = WordNetLemmatizer()
         self.mensajesTwitter['TWEET'] = self.mensajesTwitter['TWEET'].apply(lambda mensaje: ' '.join([self.lemmatizer.lemmatize(palabra) for palabra in mensaje.split(' ')]))
 
-    def aprendizaje_bayesiano(self):
+    def dividir_dataset(self):
+        '''
+        Separamos nuestro dataset en variables de entrenamiento y de test (con un 80% y 20% respectivamente)
+        '''
 
-        # Separamos nuestro dataset en variables de entrenamiento y de test (con un 80% y 20% respectivamente)
-        X_train, X_test, y_train, y_test = train_test_split(self.mensajesTwitter['TWEET'].values,  self.mensajesTwitter['CREENCIA'].values,test_size=0.2)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.mensajesTwitter['TWEET'].values,  self.mensajesTwitter['CREENCIA'].values,test_size=0.2)
+
+    def aprendizaje_bayesiano(self):
+        '''Entrenamos nuestro modelo siguiendo el modelo bayesiano
+        '''
 
         # Pipeline canaliza diferentes funciones de aprendizaje:
         #   1 - CountVectorizer: Crea una matriz con el numero de veces que aparece cada palabra
@@ -75,38 +92,66 @@ class Predicciones_cambio_climatico:
         #
         #   3 - MultinomialNB: Uso del algoritmo bayesiano ingenuo múltiple.
 
-        etapas_aprendizaje = Pipeline([('frequencia', CountVectorizer()),('tfidf', TfidfTransformer()),('algoritmo', MultinomialNB())])
+        etapas_aprendizaje = Pipeline([('frequencia', CountVectorizer()), ('tfidf', TfidfTransformer()), ('algoritmo', MultinomialNB())])
 
         # Ajustamos el modelo a nuestras variables de entrenamiento
-        self.modelo = etapas_aprendizaje.fit(X_train,y_train)
-
-        # classification_report: Printeamos un grafico que nos muestra algunas características de nuestro modelo
-        # Observamos la precisión y la validez del mismo mediante la función predict y nuestras variables de testeo
-        print(classification_report(y_test, self.modelo.predict(X_test), digits=4))
+        # .fit() es el responsable el responsable de entrenar el modelo
+        self.modelo = etapas_aprendizaje.fit(self.X_train, self.y_train)
 
 
-    # Support Vector Machine (algoritmo de aprendizaje supervisado)
-    def aprendizaje_svm(self, parametroC = 2):
+    def buscarC_optimo(self, X_train, y_train, etapas_aprendizaje):
+        '''Buscamos el numero de clusters óptimos para nuestro modelo
+
+        Args:
+            X_train (pd.Dataframe): Variables independientes del entrenamiento
+            y_train (np.array): Variable dependiente de entrenamiento
+            etapas_aprendizaje (Pipeline): Pasos en el aprendizaje
+
+        Returns:
+            int: Numero de clusters optimos
+        '''
+
+        parametrosC = {'algoritmo__C':(1,2,4,5,6,7,8,9,10,11,12)}
+        busquedaCOptimo = GridSearchCV(etapas_aprendizaje, parametrosC, cv = 2)
+        busquedaCOptimo.fit(X_train,y_train)
+
+        return list(busquedaCOptimo.best_params_.values())[0]
+
+
+    def aprendizaje_svm(self):
+        '''Entrenamos nuestro modelo utilizando el modelo Support Vector Machine (algoritmo de aprendizaje supervisado)
+        '''
 
         # Separamos nuestro dataset igual que antes
-        X_train, X_test, y_train, y_test = train_test_split(self.mensajesTwitter['TWEET'].values,  self.mensajesTwitter['CREENCIA'].values,test_size=0.2)
+        self.dividir_dataset()
 
         # Canalizamos nuevamente mediante pipeline, sin embargo, ahora indicamos el nuevo aprendizaje que vamos a seguir(SVM)
         etapas_aprendizaje = Pipeline([('frequencia', CountVectorizer()),('tfidf', TfidfTransformer()),('algoritmo', svm.SVC(kernel = 'linear', C = 2))])
-        self.modelo = etapas_aprendizaje.fit(X_train,y_train)
-        print(classification_report(y_test, modelo.predict(X_test), digits=4))
 
+        # Calculamos el numero optimo de clusters
+        n_clusters = self.buscarC_optimo(self.X_train, self.y_train, etapas_aprendizaje)
 
-    def buscarC_optimo
+        # Entrenamos nuevamente el modelo, solo que esta vez nos aseguramos de que tiene el numero correcto de clusters
+        etapas_aprendizaje = Pipeline([('frequencia', CountVectorizer()),('tfidf', TfidfTransformer()),('algoritmo', svm.SVC(kernel='linear', C = n_clusters))])
+        self.modelo = etapas_aprendizaje.fit(self.X_train, self.y_train)
+
 
 
 
     def predecir_frase(self, frase):
+        '''Esta funcion nos va a permitir predecir a partir de una frase si una persona cree en el cambio climatico o no
+
+        Args:
+            frase (String): Frase que queremos predecir
+
+        Returns:
+            Bool: Devuelve True si cree en el cambio climatico y False el caso contrario
+        '''
         #Normalización
         frase = self.normalizacion(frase)
 
         #Eliminación de las stops words
-        frase = ' '.join([palabra for palabra in frase.split() if palabra not in (stopWords)])
+        frase = ' '.join([palabra for palabra in frase.split() if palabra not in (self.stopWords)])
 
         #Aplicación de stemming
         frase =  ' '.join([self.stemmer.stem(palabra) for palabra in frase.split(' ')])
@@ -122,19 +167,15 @@ class Predicciones_cambio_climatico:
         else:
             return False
 
-
-
-
-
-
-
-
-
     def __str__(self):
         '''Nos devuelve el print de nuestra clase
 
         Returns:
             String: Información sobre la cantidad de observaciones y su contenido
         '''
-        return f'Nuestro dataset:\n{self.mensajesTwitter.head(10)}\n Tamaño: {self.mensajesTwitter.shape[0]}'
+
+        # Classification_report: Printeamos un grafico que nos muestra algunas características de nuestro modelo
+        #         Observamos la precisión y la validez del mismo mediante la función predict y nuestras variables de testeo
+        return f'Nuestro dataset:\n{self.mensajesTwitter.head(10)}\n\nTamaño: {self.mensajesTwitter.shape[0]}\n\nNuestro reporte de clasificacion del modelo:\n{classification_report(self.y_test, self.modelo.predict(self.X_test), digits=4)}'
+
 
--- menu.py
+++ menu.py
@@ -1,4 +1,50 @@
-import determinacionOpinionesTwitter
+import determinar_opiniones
+import os
+import helpers
+
 
 def lanzar():
-    
+    while True:
+
+        print("========================")
+        print('PREPARANDO...')
+        tweets = determinar_opiniones.Predicciones_cambio_climatico('datas/calentamientoClimatico.csv')
+        tweets.preparacion()
+        tweets.dividir_dataset()
+
+        helpers.limpiar_pantalla()
+        print("========================")
+        print(" BIENVENIDO AL Manager ")
+        print(" Instrucciones: Primero seleccione un modelo y despues pruebe a introducir una frase ")
+        print("========================")
+        print("[1] Modelo bayesiano ")
+        print("[2] Modelo SVM ")
+        print("[3] Introducir frase ")
+        print("[4] Salir ")
+        print("========================")
+
+        opcion = int(input("> "))
+        helpers.limpiar_pantalla()
+
+        if opcion == 1:
+            tweets.aprendizaje_bayesiano()
+            print(tweets)
+            input("\n\nPress Enter to continue...")
+
+        if opcion == 2:
+            tweets.aprendizaje_svm()
+            print(tweets)
+            input("\n\nPress Enter to continue...")
+
+        if opcion == 3:
+            frase = input("Introduce la frase que quieres predecir:\n > ")
+            if tweets.predecir_frase(frase):
+                print(">> No cree en el calentamiento climático...")
+            else:
+                print(">> Cree en el calentamiento climático...")
+
+            input("\n\nPress Enter to continue...")
+
+        if opcion == 4:
+            break
+
